118362f4f02636c75dfd2191d5b048b2fbaec184
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-03-13 14:34:15 +0800

    week3ass3

--- exercise3/源.cpp
+++ exercise3/源.cpp
@@ -2,20 +2,36 @@
 #include <opencv.hpp>
 #include <stdlib.h>
 using namespace cv;
+using namespace std;
+String window_name = "binaryMat";
+void threshod_Mat(int th, void*data)
+{
+	Mat src = *(Mat*)(data);
+	Mat dst;
+	threshold(src, dst, th, 255, 0);
+	imshow(window_name, dst);
+}
 int main()
 {
-	Mat drawImage = Mat::zeros(Size(256, 256), CV_8UC3);
-	cv::Point pt;
-	pt.x = 10;
-	pt.y = 10;
-	circle(drawImage, pt, 5, CV_RGB(255, 0, 0), 1, 8, 0);
-	line(drawImage, Point(0), Point(256), CV_RGB(255, 0, 0), 1, 8, 0);
-	cv::Rect rect;
-	rect.x = 10;
-	rect.y = 10;
-	rect.width;
-	rect.height;
-	rectangle(drawImage, rect, CV_RGB(255, 0, 0), 1, 8, 0);
-	imshow("¡¾Í¼ÐÎ¡¿", drawImage);
-	waitKey();
+	Mat srcMat;
+	Mat gryMat;
+	int lowTh = 30;
+	int maxTh = 255;
+	srcMat= imread("D:\\1.jpg");
+	if (!srcMat.data)
+	{
+		cout << "Í¼ÏñÔØÈëÊ§°Ü" << endl;
+		return 0;
+
+	}
+	cvtColor(srcMat, gryMat, CV_BGR2GRAY);
+	imshow(window_name, gryMat);
+	createTrackbar("threshold",
+		window_name,
+		&lowTh,
+		maxTh,
+		threshod_Mat,
+		&gryMat);
+	waitKey(0);
+	return 0;
 }
