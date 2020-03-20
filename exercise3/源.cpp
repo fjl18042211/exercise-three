1e5f64eeffef37f61eefd33b5f6e044a0521f520
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-03-20 15:11:39 +0800

    week4ass3

--- exercise3/源.cpp
+++ exercise3/源.cpp
@@ -3,35 +3,42 @@
 #include <stdlib.h>
 using namespace cv;
 using namespace std;
-String window_name = "binaryMat";
-void threshod_Mat(int th, void*data)
-{
-	Mat src = *(Mat*)(data);
-	Mat dst;
-	threshold(src, dst, th, 255, 0);
-	imshow(window_name, dst);
-}
+
 int main()
 {
-	Mat srcMat;
-	Mat gryMat;
-	int lowTh = 30;
-	int maxTh = 255;
-	srcMat= imread("D:\\1.jpg");
-	if (!srcMat.data)
+	cv::Mat binaryMat;
+	cv::Mat labelMat;
+	cv::Mat statsMat;
+	cv::Mat centrMat;
+	cv::Mat resultMat;
+	cv::Mat	M1;
+	cv::Mat srcMat = imread("D:\\5.jpg", 0);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	morphologyEx(srcMat, M1, MORPH_OPEN, kernel);
+	imshow("M1", M1);
+	cv::threshold(M1, binaryMat, 0, 255, THRESH_OTSU);
+	int nComp = cv::connectedComponentsWithStats(binaryMat,
+		labelMat,
+		statsMat,
+		centrMat,
+		8,
+		CV_32S);
+	cout << "the total of connected Components = " << nComp - 1 << endl;
+	resultMat = cv::Mat::zeros(srcMat.size(), CV_8UC3);
+	for (int i = 1; i < nComp; i++)
 	{
-		cout << "Í¼ÏñÔØÈëÊ§°Ü" << endl;
-		return 0;
+		char num[10];
+		sprintf_s(num, "%d", i);
 
+		Rect bndbox;
+		bndbox.x = statsMat.at<int>(i, 0);
+		bndbox.y = statsMat.at<int>(i, 1);
+		bndbox.width = statsMat.at<int>(i, 2);
+		bndbox.height = statsMat.at<int>(i, 3);
+		rectangle(resultMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
 	}
-	cvtColor(srcMat, gryMat, CV_BGR2GRAY);
-	imshow(window_name, gryMat);
-	createTrackbar("threshold",
-		window_name,
-		&lowTh,
-		maxTh,
-		threshod_Mat,
-		&gryMat);
+	imshow("binaryMat", binaryMat);
+	imshow("results", resultMat);
 	waitKey(0);
 	return 0;
 }
